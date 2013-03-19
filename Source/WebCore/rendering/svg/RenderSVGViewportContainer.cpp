WebKit/WebKit-http

commit 0827c378f816a9644c17d3805b64c4ce74c691ed

Recompute font metrics on scale changes https://bugs.webkit.org/show_bug.cgi?id=75091
Patch by Philip Rogers <pdr@google.com> on 2012-02-23
Reviewed by Nikolas Zimmermann.

Source/WebCore:

SVG text metrics depend on the transform from renderer to the svg root
which requires that we propagate transform changes down to text.
This change adds a boolean for tracking transform changes to
SVGViewportContainers and SVGTransformableContainers, and updates
RenderSVGText::layout() to recalculate text metrics if the transform
of an ancestor has changed.

Tests: platform/mac/svg/text/text-rescale.html
       platform/mac/svg/text/text-viewbox-rescale.html
       svg/text/text-rescale.html
       svg/text/text-viewbox-rescale.html

* rendering/RenderObject.h:
(WebCore::RenderObject::isSVGTransformableContainer):
(WebCore::RenderObject::isSVGViewportContainer):
* rendering/svg/RenderSVGContainer.h:
(WebCore::RenderSVGContainer::didTransformToRootUpdate):
* rendering/svg/RenderSVGInlineText.cpp:
(WebCore::RenderSVGInlineText::computeNewScaledFontForStyle):
* rendering/svg/RenderSVGText.cpp:
(WebCore::RenderSVGText::RenderSVGText):
(WebCore::RenderSVGText::layout):
* rendering/svg/RenderSVGText.h:
(WebCore::RenderSVGText::setNeedsTextMetricsUpdate):
(RenderSVGText):
* rendering/svg/RenderSVGTransformableContainer.cpp:
(WebCore::RenderSVGTransformableContainer::RenderSVGTransformableContainer):
(WebCore::RenderSVGTransformableContainer::calculateLocalTransform):
* rendering/svg/RenderSVGTransformableContainer.h:
(WebCore::RenderSVGTransformableContainer::isSVGTransformableContainer):
(WebCore::RenderSVGTransformableContainer::didTransformToRootUpdate):
(RenderSVGTransformableContainer):
* rendering/svg/RenderSVGViewportContainer.cpp:
(WebCore::RenderSVGViewportContainer::RenderSVGViewportContainer):
(WebCore::RenderSVGViewportContainer::calcViewport):
* rendering/svg/RenderSVGViewportContainer.h:
(WebCore::RenderSVGViewportContainer::didTransformToRootUpdate):

Source/WebCore/rendering/svg/RenderSVGViewportContainer.cpp
@@ -34,6 +34,7 @@ namespace WebCore {
 
 RenderSVGViewportContainer::RenderSVGViewportContainer(SVGStyledElement* node)
     : RenderSVGContainer(node)
+    , m_didTransformToRootUpdate(false)
     , m_isLayoutSizeChanged(false)
     , m_needsTransformUpdate(true)
 {
@@ -65,6 +66,7 @@ void RenderSVGViewportContainer::calcViewport()
     m_viewport = FloatRect(svg->x().value(lengthContext), svg->y().value(lengthContext), svg->width().value(lengthContext), svg->height().value(lengthContext));
 
     if (oldViewport != m_viewport) {
+        m_didTransformToRootUpdate = true;
         setNeedsBoundariesUpdate();
         setNeedsTransformUpdate();
     }

LayoutTests/platform/chromium-linux/svg/text/text-rescale-expected.txt
@@ -1,0 +1,55 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+layer at (0,0) size 500x200
+  RenderSVGRoot {svg} at (0,13) size 405x187
+    RenderSVGContainer {g} at (0,13) size 405x100
+      RenderSVGText {text} at (0,14) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,50.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,50.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,13) size 100x100
+        RenderBlock {P} at (0,0) size 100x48 [color=#008800]
+          RenderText {#text} at (0,1) size 95x46
+            text run at (0,1) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,14) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+    RenderSVGContainer {g} at (0,63) size 405x100
+      RenderSVGText {text} at (0,64) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,100.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,100.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,63) size 100x100
+        RenderBlock {P} at (0,0) size 100x48 [color=#008800]
+          RenderText {#text} at (0,1) size 95x46
+            text run at (0,1) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,64) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+    RenderSVGContainer {g} at (0,113) size 405x87
+      RenderSVGText {text} at (0,114) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,150.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,150.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,113) size 100x100
+        RenderBlock {P} at (0,0) size 100x48 [color=#008800]
+          RenderText {#text} at (0,1) size 95x46
+            text run at (0,1) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,114) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"

LayoutTests/platform/chromium-linux/svg/text/text-viewbox-rescale-expected.txt
@@ -1,0 +1,23 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+layer at (0,0) size 200x200
+  RenderSVGRoot {svg} at (0,0) size 200x200
+    RenderSVGViewportContainer {svg} at (0,0) size 200x200
+      RenderSVGText {text} at (0,0) size 1x1 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 1x1
+          chunk 1 text run 1 at (0.30,0.30) startOffset 0 endOffset 5 width 0.26: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 1x1
+          RenderSVGInlineText {#text} at (0,0) size 1x1
+            chunk 1 text run 1 at (0.56,0.30) startOffset 0 endOffset 4 width 0.24: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+    RenderSVGViewportContainer {svg} at (0,100) size 200x100
+      RenderSVGText {text} at (0,0) size 1x1 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 1x1
+          chunk 1 text run 1 at (0.30,0.30) startOffset 0 endOffset 5 width 0.26: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 1x1
+          RenderSVGInlineText {#text} at (0,0) size 1x1
+            chunk 1 text run 1 at (0.56,0.30) startOffset 0 endOffset 4 width 0.24: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0

LayoutTests/platform/mac/svg/text/text-rescale-expected.txt
@@ -1,0 +1,55 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+layer at (0,0) size 500x200
+  RenderSVGRoot {svg} at (0,13) size 405x187
+    RenderSVGContainer {g} at (0,13) size 405x100
+      RenderSVGText {text} at (0,14) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,50.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,50.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,13) size 100x100
+        RenderBlock {P} at (0,0) size 100x46 [color=#008800]
+          RenderText {#text} at (0,0) size 95x46
+            text run at (0,0) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,14) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+    RenderSVGContainer {g} at (0,63) size 405x100
+      RenderSVGText {text} at (0,64) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,100.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,100.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,63) size 100x100
+        RenderBlock {P} at (0,0) size 100x46 [color=#008800]
+          RenderText {#text} at (0,0) size 95x46
+            text run at (0,0) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,64) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+    RenderSVGContainer {g} at (0,113) size 405x87
+      RenderSVGText {text} at (0,114) size 200x46 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,0) size 105x46
+          chunk 1 text run 1 at (0.00,150.00) startOffset 0 endOffset 5 width 105.00: "PASS "
+        RenderSVGTSpan {tspan} at (0,0) size 95x46
+          RenderSVGInlineText {#text} at (105,0) size 95x46
+            chunk 1 text run 1 at (105.00,150.00) startOffset 0 endOffset 4 width 95.00: "PASS"
+        RenderSVGInlineText {#text} at (0,0) size 0x0
+      RenderSVGForeignObject {foreignObject} at (210,113) size 100x100
+        RenderBlock {P} at (0,0) size 100x46 [color=#008800]
+          RenderText {#text} at (0,0) size 95x46
+            text run at (0,0) width 95: "PASS"
+      RenderSVGViewportContainer {svg} at (310,114) size 95x46
+        RenderSVGText {text} at (0,1) size 95x46 contains 1 chunk(s)
+          RenderSVGInlineText {#text} at (0,0) size 95x46
+            chunk 1 text run 1 at (0.00,37.00) startOffset 0 endOffset 4 width 95.00: "PASS"

LayoutTests/platform/mac/svg/text/text-rescale.html
@@ -1,0 +1,55 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<!-- Test that a script can change the scale of text from very small to something visible -->
+<!-- see: https://bugs.webkit.org/show_bug.cgi?id=75091 -->
+<!-- If this test passes there should be 3 rows and 4 columns of "PASS" -->
+<script src="../../fast/repaint/resources/repaint.js"></script>
+</head>
+<body onload="runRepaintTest()">
+<svg style="position: absolute; top: 0px; left: 0px; width: 500px; height: 200px">
+    <g id="text1g" transform="scale(0.001)">
+        <text x="0" y="50" font-size="40" fill="#080">
+            PASS
+            <tspan fill="#080"> PASS</tspan>
+        </text>
+        <foreignObject x="210" y="13" width="100" height="100">
+            <body><p style="font-size: 40px; color: #080;">PASS</p></body>
+        </foreignObject>
+        <svg x="310" y="13" width="100" height="100">
+            <text x="0" y="37" font-size="40" fill="#080">PASS</text>
+        </svg>
+    </g>
+    <g id="text2g" transform="scale(1)">
+        <text x="0" y="100" font-size="40" fill="#080">
+            PASS
+            <tspan fill="#080"> PASS</tspan>
+        </text>
+        <foreignObject x="210" y="63" width="100" height="100">
+            <body><p style="font-size: 40px; color: #080;">PASS</p></body>
+        </foreignObject>
+        <svg x="310" y="63" width="100" height="100">
+            <text x="0" y="37" font-size="40" fill="#080">PASS</text>
+        </svg>
+    </g>
+    <g id="text3g" transform="scale(0.03)">
+        <text x="0" y="150" font-size="40" fill="#080">
+            PASS
+            <tspan fill="#080"> PASS</tspan>
+        </text>
+        <foreignObject x="210" y="113" width="100" height="100">
+            <body><p style="font-size: 40px; color: #080;">PASS</p></body>
+        </foreignObject>
+        <svg x="310" y="113" width="100" height="100">
+            <text x="0" y="37" font-size="40" fill="#080">PASS</text>
+        </svg>
+    </g>
+</svg>
+<script>
+    function repaintTest() {
+        document.getElementById("text1g").setAttribute('transform', 'scale(1)');
+        document.getElementById("text2g").setAttribute('transform', 'scale(1)');
+        document.getElementById("text3g").setAttribute('transform', 'scale(1)');
+    }
+</script>
+</body>
+</html>

qt/qtwebkit

commit a0dd077c811c3449eeb879541973fa8e1b255cee

Prevent skipped repaints for children of inner SVG elements
https://bugs.webkit.org/show_bug.cgi?id=108429

Reviewed by Eric Seidel.

Source/WebCore:

This patch fixes a bug caused by r108699 and r133786 where we would not repaint children
of inner SVG elements because "m_didTransformToRootUpdate" was never reset on viewport
containers. The stale m_didTransformToRootUpdate variable caused us to skip child repaints.

I verified that the Robohornet SVG benchmark performance gains in r133786 are not regressed
with this patch.

Test: svg/repaint/svgsvgelement-repaint-children.html

* rendering/svg/RenderSVGViewportContainer.cpp:
(WebCore::RenderSVGViewportContainer::calcViewport):

    This can be removed because setNeedsTransformUpdate() will set m_needsTransformUpdate.

(WebCore::RenderSVGViewportContainer::calculateLocalTransform):

    This change is straightforward and is similar to the equivalent assignment in
    RenderSVGTransformableContainer::calculateLocalTransform().

Change-Id: I505d852152ef1a8e084a002e65bb5e06aeca4988
git-svn-id: http://svn.webkit.org/repository/webkit/trunk@141645 268f45cc-cd09-0410-ab3c-d52691b4dbfc
Reviewed-by: Jocelyn Turcotte <jocelyn.turcotte@digia.com>

Source/WebCore/rendering/svg/RenderSVGViewportContainer.cpp
@@ -114,7 +114,6 @@ void RenderSVGViewportContainer::calcViewport()
     }
 
     if (oldViewport != m_viewport) {
-        m_didTransformToRootUpdate = true;
         setNeedsBoundariesUpdate();
         setNeedsTransformUpdate();
     }
@@ -122,6 +121,7 @@ void RenderSVGViewportContainer::calcViewport()
 
 bool RenderSVGViewportContainer::calculateLocalTransform() 
 {
+    m_didTransformToRootUpdate = m_needsTransformUpdate || SVGRenderSupport::transformToRootChanged(parent());
     if (!m_needsTransformUpdate)
         return false;
     