d92f0c4f34bed79d8296126016330c9df138c54b
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-06-03 13:13:26 +0000

    [Qt] Re-enable plugins on Mac.
    https://bugs.webkit.org/show_bug.cgi?id=116622
    Reviewed by Tor Arne Vestbø.
    
    Source/WebCore:
    
    * platform/FileSystem.h:
    * platform/qt/FileSystemQt.cpp:
    (WebCore::unloadModule):
      - Q_WS_* aren't defined since Qt5, use Q_OS_MACX instead.
    * plugins/mac/PluginPackageMac.cpp:
    (WebCore::PluginPackage::fetchInfo):
    (WebCore::PluginPackage::load):
      - createCFString now does the adoption itself.
    * plugins/mac/PluginViewMac.mm:
      - Add missing include.
    
    Source/WebKit/qt:
    
    * WebCoreSupport/ChromeClientQt.cpp:
    (WebCore::ChromeClientQt::windowResizerRect):
      - Add missing parenthese.
    
    Tools:
    
    Plugins on Mac were only enabled if !embedded but the later was set if
    QPA is enabled, which is the default since Qt 5.0.
    
    Remove references to 'embedded' and fix various build issues,
    PluginViewMac.mm and PluginPackageMac.cpp haven't been compiled since a few
    months.
    
    * DumpRenderTree/qt/TestNetscapePlugin/TestNetscapePlugin.pro:
    * qmake/mkspecs/features/features.prf:
    * qmake/mkspecs/features/unix/default_pre.prf:
    
    Task-number: QTBUG-35899
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@151109 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    Change-Id: I49692a234a66c205099c5dde8eb24ffb6eadba0f
    Reviewed-by: Jocelyn Turcotte <jocelyn.turcotte@digia.com>

--- Source/WebCore/plugins/mac/PluginPackageMac.cpp
+++ Source/WebCore/plugins/mac/PluginPackageMac.cpp
@@ -138,8 +138,8 @@ bool PluginPackage::fetchInfo()
     if (mimeTypesFileName && CFGetTypeID(mimeTypesFileName.get()) == CFStringGetTypeID()) {
 
         WTF::RetainPtr<CFStringRef> fileName = (CFStringRef)mimeTypesFileName.get();
-        WTF::RetainPtr<CFStringRef> homeDir = adoptCF(homeDirectoryPath().createCFString());
-        WTF::RetainPtr<CFStringRef> path = adoptCF(CFStringCreateWithFormat(0, 0, CFSTR("%@/Library/Preferences/%@"), homeDir.get(), fileName.get()));
+        WTF::RetainPtr<CFStringRef> homeDir = homeDirectoryPath().createCFString();
+        WTF::RetainPtr<CFStringRef> path(AdoptCF, CFStringCreateWithFormat(0, 0, CFSTR("%@/Library/Preferences/%@"), homeDir.get(), fileName.get()));
 
         WTF::RetainPtr<CFDictionaryRef> plist = readPListFile(path.get(), /*createFile*/ false, m_module);
         if (plist) {
@@ -255,8 +255,8 @@ bool PluginPackage::load()
         return true;
     }
 
-    WTF::RetainPtr<CFStringRef> path = adoptCF(m_path.createCFString());
-    WTF::RetainPtr<CFURLRef> url = adoptCF(CFURLCreateWithFileSystemPath(kCFAllocatorDefault, path.get(),
+    WTF::RetainPtr<CFStringRef> path = m_path.createCFString();
+    WTF::RetainPtr<CFURLRef> url(AdoptCF, CFURLCreateWithFileSystemPath(kCFAllocatorDefault, path.get(),
                                                                         kCFURLPOSIXPathStyle, false));
     m_module = CFBundleCreate(NULL, url.get());
     if (!m_module || !CFBundleLoadExecutable(m_module)) {
