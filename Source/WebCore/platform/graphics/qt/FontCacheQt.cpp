c31ff44d3a17fab37754acb2a0a29cd6eba10699
Fabienne Semeria <fsemeria@nds.com>
2013-07-11 14:52:30 +0200

    [Qt] memory leak in WebCore::FontCache::getLastResortFallbackFont
    https://bugs.webkit.org/show_bug.cgi?id=118532
    
    Patch by Fabienne Semeria <fsemeria@nds.com> on 2013-07-11
    Reviewed by Allan Sandfeld Jensen.
    
    * platform/graphics/qt/FontCacheQt.cpp:
    (WebCore::FontCache::getLastResortFallbackFont):
    
    Change-Id: Ic647594b3d6eaaeb036a3d3d88f2eb533cffedb7
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@152563 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    Reviewed-by: Fabienne Semeria <fsemeria@nds.com>
    Reviewed-by: Jocelyn Turcotte <jocelyn.turcotte@digia.com>

--- Source/WebCore/platform/graphics/qt/FontCacheQt.cpp
+++ Source/WebCore/platform/graphics/qt/FontCacheQt.cpp
@@ -81,7 +81,7 @@ PassRefPtr<SimpleFontData> FontCache::getSimilarFontPlatformData(const Font& fon
 PassRefPtr<SimpleFontData> FontCache::getLastResortFallbackFont(const FontDescription& fontDescription, ShouldRetain shouldRetain)
 {
     const AtomicString fallbackFamily = QFont(fontDescription.family().family()).lastResortFamily();
-    return getCachedFontData(new FontPlatformData(fontDescription, fallbackFamily), shouldRetain);
+    return getCachedFontData(fontDescription, fallbackFamily, false, shouldRetain);
 }
 
 void FontCache::getTraitsInFamily(const AtomicString&, Vector<unsigned>&)
