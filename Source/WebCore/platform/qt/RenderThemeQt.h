1e0e8bbbe3c588d8a8340a26fcf124d50f722633
Allan Sandfeld Jensen <allan.jensen@digia.com>
2014-07-23 15:51:56 +0200

    Show placeholder text when focused
    Change the default behavior of placeholder text, so that it continues
    to be shown when the element has focus but is still empty.
    
    Task-number: QTBUG-40171
    Change-Id: Ide37c3b28488cb6c24f7823a41fb62c8920a71f6
    Reviewed-by: Michael Bruning <michael.bruning@digia.com>

--- Source/WebCore/platform/qt/RenderThemeQt.h
+++ Source/WebCore/platform/qt/RenderThemeQt.h
@@ -173,6 +173,8 @@ protected:
 
     virtual QSharedPointer<StylePainter> getStylePainter(const PaintInfo&) = 0;
 
+    virtual bool shouldShowPlaceholderWhenFocused() const OVERRIDE { return true; }
+
     bool supportsFocus(ControlPart) const;
 
     IntRect convertToPaintingRect(RenderObject* inputRenderer, const RenderObject* partRenderer, IntRect partRect, const IntRect& localOffset) const;
