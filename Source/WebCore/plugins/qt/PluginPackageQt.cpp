ea590d74eae21dd70b189e0b8ba4bfb6a9bddb94
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-06-01 12:34:00 +0200

    Blacklist vlc plugin
    The VLC plugin may load a plugins of its own that uses Qt4.
    
    Change-Id: I5229a360f7d08d024fb93df1a2993fd7c0176848
    Task-number: QTBUG-45431
    Reviewed-by: Simon Hausmann <simon.hausmann@theqtcompany.com>

--- Source/WebCore/plugins/qt/PluginPackageQt.cpp
+++ Source/WebCore/plugins/qt/PluginPackageQt.cpp
@@ -147,7 +147,8 @@ bool PluginPackage::isPluginBlacklisted()
     // TODO: enumerate all plugins that are incompatible with Qt5.
     const QLatin1String pluginBlacklist[] = {
         QLatin1String("skypebuttons"),
-        QLatin1String("libkpartsplugin")
+        QLatin1String("libkpartsplugin"),
+        QLatin1String("libvlcplugin")
     };
 
     QString baseName = QFileInfo(static_cast<QString>(m_path)).baseName();
